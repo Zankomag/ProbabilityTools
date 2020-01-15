d6b0006ddea76f732b1e045775d8c52b75523935
Adam Evans <Zankomag@gmail.com>
2020-01-15 20:08:54 +0200

    Remove FastRandom
    System.Random will be used instead of FastRandom
    FastRandom has bad range spreading of numbers

--- Probability.cs
+++ Probability.cs
@@ -1,10 +1,11 @@
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace Generic {
 	public static class Probability {
 
-		private static readonly FastRandom random = new FastRandom();
+		private static readonly Random random = new Random();
 
 		public static void ReinitialiseRandom(int seed) {
 			random.Reinitialise(seed);
